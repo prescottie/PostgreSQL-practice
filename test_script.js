58e19df3b3fda90a2913cf4e5f1b6a78c4174465
Scotty <scott.hagan@aol.com>
2017-09-26 19:23:51 +0000

    added test_script to test node pg connect function

---
+++ test_script.js
@@ -0,0 +1,35 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
+});
+
+const name = process.argv[2];
+
+client.connect(err => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  console.log("Searching...");
+  client.query(
+    "SELECT * FROM famous_people WHERE first_name LIKE $1::text OR last_name LIKE $1::text",
+    [name],
+    (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      console.log(`Found 1 person(s) by the name '${name}'`);
+      console.log(
+        `- ${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0]
+          .last_name}, born  ${result.rows[0].birthdate}`
+      );
+      client.end();
+    }
+  );
+});
